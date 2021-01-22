ba211bb84a6af941398d2a24f98fe45f98361211
andrea <andreacazzola90@gmail.com>
2021-01-22 17:33:11 +0100

    add scripts

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,95 +1,157 @@
-import ScrollMagic from "scrollmagic"; // Or use scrollmagic-with-ssr to avoid server rendering problems
+/* import ScrollMagic from "scrollmagic";  */// Or use scrollmagic-with-ssr to avoid server rendering problems
+import * as $ from 'jquery';
 
-
-import { gsap, TweenMax, TimelineMax } from "gsap"; // Also works with TweenLite and TimelineLite
-import { ScrollMagicPluginGsap } from "scrollmagic-plugin-gsap";
+/* import { gsap, TweenMax, TimelineMax } from "gsap"; // Also works with TweenLite and TimelineLite
+import { ScrollMagicPluginGsap } from "scrollmagic-plugin-gsap"; */
 import '../styles/index.scss';
 
-ScrollMagicPluginGsap(ScrollMagic, TweenMax, TimelineMax);
+/* import "scrollmagic/scrollmagic/minified/plugins/debug.addIndicators.min";
+
+ScrollMagicPluginGsap(ScrollMagic, TweenMax, TimelineMax); */
 
 if (process.env.NODE_ENV === 'development') {
   require('../index.html');
 }
 
+/* //SCROLLMAGIC
+const controller = new ScrollMagic.Controller();
 
-// var controller = new ScrollMagic.Controller();
-// var scene = new ScrollMagic.Scene({
-//   triggerElement: ".section-trigger"
-// })
-//   .setTween(".image-animation", { rotation: 27, x: 100 }) // trigger a TweenMax.to tween
-//   .addTo(controller);
-
-//selettore della sezione
-// var controller = new ScrollMagic.Controller();
-
-// var els = document.getElementsByClassName("container-vh");
-
-// Array.from(els).forEach((elem) => {
-
-//   // Init ScrollMagic Controller
-
-//   // Create Animations
-
-//   //selettore dell'tag da animare
-//   var title = elem.querySelector('img');
-//   let video = elem.querySelector('.video-animation');
-
-//   // text = $(elem).find('p'),
-//   // btn = $(elem).find('a');
+//Text Animation
 
 
-//   var tl = new TimelineMax();
+$(".box-container").each(function () {
 
-//   // var tl = TweenMax.to(title, 1, { x: '50vh', rotation: 360 });
-//   tl.fromTo(title, 0.4, { y: '60px', opacity: 0 }, { y: 0, opacity: 1 }, "start")
-//     .fromTo(title, 1, { x: '100px', opacity: 0.5 }, { x: '50px', opacity: .7 });
+  //Scenes
+  let scene = new ScrollMagic.Scene({
+    triggerElement: '.box-container .c-video',
+    triggerHook: ("onLeave")
+  })
+    .setPin('.box-container .c-video')
+    .addTo(controller);
 
-//   // tl.fromTo(title, 0.9, { y: '40px', opacity: .5, rotation: 50 }, { y: 0, opacity: 1, rotation: 0 });
-//   // .fromTo(text, 0.4, { y: '60px', opacity: 0 }, { y: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
-//   // .fromTo(btn, 0.4, { y: '100px', opacity: 0 }, { y: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
+  const video = this.querySelector("video");
 
-//   // Create the Scene and trigger when visible
-//   var scene1 = new ScrollMagic.Scene({
-//     triggerElement: elem
+  //Video Animation
+  let accelamount = 0.3;
+  let scrollpos = 0;
+  let delay = 0;
 
-//   })
-//     .setTween(tl)
-//     .addTo(controller);
+  scene.on("update", e => {
+    scrollpos = e.scrollPos / 1000;
+  });
 
-// });
+  setInterval(() => {
+    delay += (scrollpos * 4 - delay) * accelamount;
+
+    video.currentTime = delay;
+  }, 40);
+  console.log(video);
+
+});
+ */
 
 
 
 
-const intro = document.querySelector(".intro");
-const video = intro.querySelector("video");
-const text = intro.querySelector("h1");
+
+
+
+/* var ctrl = new ScrollMagic.Controller();
+
+// Create scenes in jQuery each() loop
+$(".box-container .c-video").each(function (i) {
+
+  if ($(this).find(".c-video")) {
+    var outer = $(this).parent();
+    console.log({ outer });
+    var video = $(this).find('video')
+    var videoId = '#';
+    videoId += this.id;
+    console.log(videoId);
+
+    var video = $(this).find("video");
+    let height = $(this).outerHeight();
+    var tl = new TimelineMax();
+
+    tl.fromTo(video, 3, { opacity: 0.2 }, { opacity: 1 });
+
+    let scene = new ScrollMagic.Scene({
+      triggerElement: this,
+      triggerHook: 0,
+      offset: -200,
+      duration: height,
+      reverse: true
+    })
+      .setPin(videoId)
+      .setTween(tl)
+      .addIndicators({
+        colorTrigger: "white",
+        colorStart: "white",
+        colorEnd: "white",
+        indent: 40
+      })
+      .addTo(ctrl);
+
+
+    //Video Animation
+    let accelamount = 0.3;
+    let scrollpos = 0;
+    let delay = 0;
+
+    scene.on("update", e => {
+      scrollpos = e.scrollPos / 1000;
+    });
+
+    setInterval(() => {
+      delay += (scrollpos * 4 - delay) * accelamount;
+
+      video.currentTime = delay;
+    }, 40);
+
+  }
+
+
+});
+ */
+
+
+const boxContianers = document.querySelectorAll(".box-container");
+const boxsVideo = [];
+
+function checkVideo(x) {
+  return x.children >= 18;
+}
+
+boxContianers.forEach(x => {
+  var nodoChild = x.children
+  boxsVideo.push(x);
+});
+console.log({ boxContianers });
+console.log({ boxsVideo });
+console.log(typeof boxContianers);
+
+
+const cVideo = document.querySelector(".c-video");
+console.log({ cVideo });
+const video = cVideo.querySelector("video");
+console.log({ video });
 //END SECTION
-const section = document.querySelector("section");
-const end = section.querySelector("h1");
+let section = cVideo.parentNode;
+console.log({ section });
 
 //SCROLLMAGIC
 const controller = new ScrollMagic.Controller();
 
+var tl = new TimelineMax();
+
 //Scenes
 let scene = new ScrollMagic.Scene({
   duration: 2520,
-  triggerElement: intro,
+  triggerElement: section,
   triggerHook: 0
 })
   .addIndicators()
-  .setPin(intro)
-  .addTo(controller);
-
-//Text Animation
-const textAnim = gsap.fromTo(text, 3, { opacity: 1 }, { opacity: 0 });
-
-let scene2 = new ScrollMagic.Scene({
-  duration: 3000,
-  triggerElement: intro,
-  triggerHook: 0
-})
-  .setTween(textAnim)
+  .setPin(section)
   .addTo(controller);
 
 //Video Animation
@@ -110,30 +172,45 @@ setInterval(() => {
 console.log(video);
 
 
-// var frameNumber = 0, // start video at frame 0
-//   // lower numbers = faster playback
-//   playbackConst = 1000,
-//   // get page height from video duration
-//   setHeight = document.querySelector(".section-video"),
-//   // select video element         
-//   vid = document.getElementById('v0');
-// // var vid = $('#v0')[0]; // jquery option
-
-// // dynamically set the page height according to video length
-// vid.addEventListener('loadedmetadata', function () {
-//   console.log(vid.duration);
-//   setHeight.style.height = Math.floor(vid.duration) + playbackConst + "px";
-// });
 
 
-// // Use requestAnimationFrame for smooth playback
-// function scrollPlay() {
-//   var videoHeight = document.querySelector(".section-video");
-//   console.log(videoHeight);
-//   var frameNumber = 0;
-//   console.log(frameNumber);
-//   vid.currentTime = frameNumber;
-//   window.requestAnimationFrame(scrollPlay);
-// }
 
-// window.requestAnimationFrame(scrollPlay);
+
+
+
+/*
+const intro = document.querySelector(".box-container");
+const video = intro.querySelector("video");
+const text = intro.querySelector("h1");
+
+console.log({ intro, video, text });
+//END SECTION
+const section = document.querySelector("section");
+const end = section.querySelector("h1");
+
+//SCROLLMAGIC
+const controller = new ScrollMagic.Controller();
+
+//Scenes
+let scene = new ScrollMagic.Scene({
+  duration: 2520,
+  triggerElement: intro,
+  triggerHook: 0
+})
+  .setPin(intro)
+  .addTo(controller);
+
+//Text Animation
+const textAnim = gsap.fromTo(video, 3, { opacity: 1 }, { opacity: 0 });
+
+let scene2 = new ScrollMagic.Scene({
+  duration: 3000,
+  triggerElement: intro,
+  triggerHook: 0
+})
+  .setTween(textAnim)
+  .addTo(controller); */
+
+
+
+
